88fcdc94c3bd0ad2a45983c59c40cafd38e0abd0
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-12 22:03:12 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,8 +2,8 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
-  create_table :artists do |t|
-      t.string :first_name
-      t.string :last_name
+  create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
     end
 end
