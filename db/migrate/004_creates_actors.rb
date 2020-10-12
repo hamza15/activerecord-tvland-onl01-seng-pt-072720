f4806197300afba6394958e0237e77df9be2a8b9
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-12 22:01:55 +0000

    Automatically backed up by Learn

--- db/migrate/004_creates_actors.rb
+++ db/migrate/004_creates_actors.rb
@@ -1,4 +1,4 @@
-class CreateActors < ActiveRecord::Migration[5.1]
+class CreatesActors < ActiveRecord::Migration[5.1]
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
