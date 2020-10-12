bbcc853f0751cdf20692726a037325c527df6d53
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-12 22:09:38 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,10 +3,10 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
   #a new migration, and then in the change method,
   #we make the change.
   def change
-  create_table :characters do |t|
-      t.string :name
-      t.integer :actor_id
-      t.integer :show_id
-    end
+    create_table :characters do |t|
+        t.string :name
+        t.integer :actor_id
+        t.integer :show_id
+      end
   end
 end
