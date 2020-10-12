3c4780688ffd0ef6939fa82d84a666ccf8d235d3
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-12 22:05:56 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_cacthphrase_to_characters.rb
+++ db/migrate/006_add_cacthphrase_to_characters.rb
@@ -1,10 +1,5 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
-  #to make an additive change to a schema we create
-  #a new migration, and then in the change method,
-  #we make the change.
-  create_table :characters do |t|
-      t.string :name
-      t.integer :actor_id
-      t.integer :show_id
+  def change
+      add_column :shows, :network_id, :integer
     end
 end
